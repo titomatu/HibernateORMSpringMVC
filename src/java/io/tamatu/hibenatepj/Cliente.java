35f6cfd90f1227ed8558f8f39410b205edc26953
titomatu <tamatu@gmail.com>
2021-09-04 15:48:14 -0500

    One To One ORM Relashionship

---
+++ src/java/io/tamatu/hibenatepj/Cliente.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.tamatu.hibenatepj;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author titonitola-maturana
+ */
+@Entity
+@Table(name="cliente")
+public class Cliente {
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String apellido, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public DetalleCliente getDetalleCliente() {
+        return detalleCliente;
+    }
+
+    public void setDetalleCliente(DetalleCliente detalleCliente) {
+        this.detalleCliente = detalleCliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion + ", detalleCliente=" + detalleCliente + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id; 
+    @Column(name="nombre")
+    private String nombre;
+    @Column(name="apellido")
+    private String apellido;
+    @Column(name="direccion")
+    private String direccion;
+    
+    @OneToOne(cascade=CascadeType.ALL) //Tipo de relación y cascada
+    @JoinColumn(name="id") //ColumnaJoin
+    private DetalleCliente detalleCliente;
+}
---
+++ src/java/io/tamatu/hibenatepj/DetalleCliente.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.tamatu.hibenatepj;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author titonitola-maturana
+ */
+@Entity
+@Table(name="detalle_cliente")
+public class DetalleCliente {
+
+    public DetalleCliente() {
+    }
+
+    public DetalleCliente(String web, String telefono, String comentarios) {
+        this.web = web;
+        this.telefono = telefono;
+        this.comentarios = comentarios;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalleCliente{" + "id=" + id + ", web=" + web + ", telefono=" + telefono + ", comentarios=" + comentarios + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 19 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DetalleCliente other = (DetalleCliente) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="web")
+    private String web;
+    @Column(name="tfno")
+    private String telefono;
+    @Column(name="comentarios")
+    private String comentarios;
+}
